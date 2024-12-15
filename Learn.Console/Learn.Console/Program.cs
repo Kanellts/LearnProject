d767b84b3c9d8928a77b6375f97914ec960aead0
Kanellts <67736037+Kanellts@users.noreply.github.com>
2024-12-15 19:25:00 +0200

    Tweaking the db connections

--- Learn.Console/Learn.Console/Program.cs
+++ Learn.Console/Learn.Console/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Npgsql;
+using System;
+using System.Linq;
+
+// namespace Learn.Console;
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Host=localhost;Port=user_port;Database=database_name;User Id=username;Password=password;";
+
+        using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+
+        using (var db = new StudentsContext())
+        {
+            Console.Write("Enter credentials for a new student:");
+            var id = Convert.ToInt32(Console.ReadLine());
+            var name = Console.ReadLine();
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            var student = new Student(id,name,age);
+        }
+        // using NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM students", connection);
+
+        // using NpgsqlDataReader reader = cmd.ExecuteReader();
+
+        // while (reader.Read())
+        // {
+        //     Console.WriteLine(reader["column_name"]);
+        //     // Use the fetched results
+        // }
+    }
+}
--- Learn.Console/Learn.Console/model.cs
+++ Learn.Console/Learn.Console/model.cs
@@ -6,19 +6,32 @@ public class StudentsContext : DbContext
 {
     public DbSet<Student> Students { get; set; }
 
-    // public string DbPath { get; }
-
+    public string DbPath { get; }
+    // public StudentsContext()
+    // {
+    //     var folder = Environment.SpecialFolder.LocalApplicationData;
+    //     var path = Environment.GetFolderPath(folder);
+    //     DbPath = System.IO.Path.Join(path, "mydatabase.db");
+    // }
     public StudentsContext() 
     {
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql(@"Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase");
+        => optionsBuilder.UseNpgsql(@"Host=localhost;Username=mylogin;Password=mypass;Database=mydatabase");
+    // protected override void OnConfiguring(DbContextOptionsBuilder options)
+    //     => options.UseNpgsql($"Data Source={DbPath}");
 }
 
 public class Student
 {
     public int Id { get; set;}
-    public string name { get; set; }
-    public int age { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Student(int id, string name, int age) {
+        this.Id = id;
+        this.Name = name;
+        this.Age = age;
+    }
 }
