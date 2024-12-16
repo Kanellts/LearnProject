989388aac142341fdd64156907e3a3489b591505
Kanellts <67736037+Kanellts@users.noreply.github.com>
2024-12-16 15:35:56 +0200

    Final changes to the code. Documentation is the only thing left

--- Learn.Console/Learn.Console/obj/Debug/net8.0/Learn.Console.AssemblyInfo.cs
+++ Learn.Console/Learn.Console/obj/Debug/net8.0/Learn.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learn.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631ac9e1bcb08717794cf1734cc091ea18b29c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57446a8e1d189abf4ebf99698d5ae7301ee25d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learn.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learn.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
