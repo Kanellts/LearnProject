3c4fbe949c8c965cdb8d58bbcce9342665ab562e
Kanellts <67736037+Kanellts@users.noreply.github.com>
2025-02-06 14:45:35 +0200

    Learn.Service deleted

--- Learn.Service/IStudentsService.cs
+++
@@ -1,9 +0,0 @@
-using Learn.Core.DataAccess.Models;
-
-namespace Learn.Service {
-    public interface IStudentsService {
-        Task CreateStudentAsync(Student student);
-        Task<List<Student>> GetAllStudentsAsync();
-        Task UpdateStudentAsync(Student student);
-    }
-}
