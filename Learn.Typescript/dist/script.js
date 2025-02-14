5688a7c14d41cd23d25c4b0f74a71f3e1f8196d2
Kanellts <67736037+Kanellts@users.noreply.github.com>
2025-02-14 15:58:48 +0200

    Grid table finally works with ag-grid without errors. http-server displays now a default table, no connection with the database is established yet

--- Learn.Typescript/dist/script.js
+++ Learn.Typescript/dist/script.js
@@ -1,131 +1,124 @@
 "use strict";
-// import axios from 'axios';
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+// import { GridApi, GridOptions, createGrid, AllCommunityModule, ModuleRegistry } from '../node_modules/ag-grid-community';
+// import '../node_modules/ag-grid-community/styles/ag-grid.css'
+// import '../node_modules/ag-grid-community/styles/ag-theme-balham.css'
+const gridOptions = {
+    // Grid Options: Contains all of the Data Grid configurations
+    columnDefs: [
+        { headerName: 'Id', field: 'id', sortable: true, filter: true },
+        { headerName: 'Name', field: 'name', sortable: true, filter: true },
+        { headerName: 'Age', field: 'age', sortable: true, filter: true }
+    ],
+    rowData: [
+        { id: "1", name: "Kanellos", age: "45" },
+        { id: "2", name: "Kanell ts", age: "45" },
+        { id: "3", name: "Kanellos safas", age: "45" }
+    ]
 };
-const saveButton = document.querySelector('#btnSave');
-const deleteButton = document.querySelector('#btnDelete');
-const studentName = document.querySelector('#name');
-const studentAge = document.querySelector('#age');
-const studentsContainer = document.querySelector('#students_container');
-if (!saveButton || !deleteButton || !studentName || !studentAge || !studentsContainer) {
-    throw new Error("One or more required elements are missing in the DOM.");
-}
-function clearForm() {
-    return __awaiter(this, void 0, void 0, function* () {
-        studentName.value = '';
-        studentAge.value = '';
-        deleteButton.classList.add('hidden');
-    });
-}
-function displayStudentInForm(student) {
-    return __awaiter(this, void 0, void 0, function* () {
-        studentName.value = student.name;
-        studentAge.value = student.age.toString();
-        deleteButton.classList.remove('hidden');
-        saveButton.setAttribute('data-id', student.id.toString());
-        deleteButton.setAttribute('data-id', student.id.toString());
-    });
-}
-function getStudentById(id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const response = (yield axios.get(`https://localhost:7103/api/Students/${id}`));
-            displayStudentInForm(response.data);
-        }
-        catch (error) {
-            console.error("Error fetching students: ", error);
-        }
-    });
-}
-function populateForm(id) {
-    getStudentById(id);
-}
-function displayStudents(students) {
-    let allStudents = '';
-    students.forEach((student) => {
-        const studentElement = `
-                                    <div class="student" data-id="${student.id}">
-                                        <h3>${student.name}</h3>
-                                        <p>${student.age}</p>
-                                    </div>
-                                `;
-        allStudents += studentElement;
-    });
-    studentsContainer.innerHTML = allStudents;
-    //add event listener
-    document.querySelectorAll('.student').forEach(student => {
-        student.addEventListener('click', function () {
-            return __awaiter(this, void 0, void 0, function* () {
-                populateForm(Number(student.getAttribute("data-id")));
-            });
-        });
-    });
-}
-function getAllStudents() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const response = yield axios.get('https://localhost:7103/api/Students');
-            displayStudents(response.data);
-        }
-        catch (error) {
-            console.error("Error fetching students: ", error);
-        }
-    });
-}
-function createStudent(name, age) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield axios.post('https://localhost:7103/api/Students', { name, age }, { headers: { "content-type": "application/json" } });
-            clearForm();
-            getAllStudents();
-        }
-        catch (error) {
-            console.error("Error creating student: ", error);
-        }
-    });
-}
-function updateStudent(id, name, age) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield axios.put(`https://localhost:7103/api/Students/${id}`, { name, age }, { headers: { "content-type": "application/json" } });
-            clearForm();
-            getAllStudents();
-        }
-        catch (error) {
-            console.error("Error updating student: ", error);
-        }
-    });
-}
-saveButton.addEventListener('click', function () {
-    const id = saveButton.dataset.id;
-    if (id) {
-        updateStudent(Number(id), studentName.value, Number(studentAge.value));
-    }
-    else {
-        createStudent(studentName.value, Number(studentAge.value));
-    }
-});
-function deleteStudent(id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield axios.delete(`https://localhost:7103/api/Students/${id}`);
-            clearForm();
-            getAllStudents();
-        }
-        catch (error) {
-            console.error("Error deleting student: ", error);
-        }
-    });
-}
-deleteButton.addEventListener('click', function () {
-    const id = deleteButton.dataset.id;
-    deleteStudent(Number(id));
-});
-getAllStudents();
+const studentsGrid = document.querySelector("#studentsGrid");
+const gridApi = agGrid.createGrid(studentsGrid, gridOptions);
+// document.addEventListener('DOMContentLoaded', async function() {
+//     if(en {
+//         new (window as any).agGrid.Grid(en gridOptions);
+//         getAllStudents();
+//     }
+//     else {
+//         console.error("AG Grid container not found");
+//     }
+// });
+// async function clearForm() {
+//     studentName!.value = '';
+//     studentAge!.value = '';
+//     deleteButton!.classList.add('hidden');
+// }
+// async function displayStudentInForm(student: { name: string; age: number; id: number; }) {
+//     studentName!.value = student.name;
+//     studentAge!.value = student.age.toString();
+//     deleteButton!.classList.remove('hidden');
+//     saveButton!.setAttribute('data-id',student.id.toString());
+//     deleteButton!.setAttribute('data-id',student.id.toString());
+// }
+// async function getStudentById(id: number) {
+//     try {
+//         const response = (await axios.get<Student>(`https://localhost:7103/api/Students/${id}`));
+//         displayStudentInForm(response.data);
+//     }
+//     catch(error) {
+//         console.error("Error fetching students: ", error);
+//     }
+// }
+// function populateForm(id: number) {
+//     getStudentById(id);
+// }
+// function displayStudents(students: Student[]) {
+//     let allStudents = '';
+//     students.forEach((student) => {
+//         const studentElement = `
+//                                     <div class="student" data-id="${student.id}">
+//                                         <h3>${student.name}</h3>
+//                                         <p>${student.age}</p>
+//                                     </div>
+//                                 `;
+//         allStudents += studentElement        
+//     });
+//     studentsContainer!.innerHTML = allStudents;
+//     //add event listener
+//     document.querySelectorAll('.student').forEach(student => {
+//         student.addEventListener('click', async function() {
+//             populateForm(Number(student!.getAttribute("data-id")));
+//         });
+//     });
+// }
+// async function getAllStudents() {
+//     try {
+//         const response = await axios.get<Student[]>('https://localhost:7103/api/Students');
+//         displayStudents(response.data);
+//     } 
+//     catch(error) {
+//         console.error("Error fetching students: ", error);
+//     }
+// }
+// async function createStudent(name: string, age: number) {
+//     try {
+//         await axios.post<StudentDTO>('https://localhost:7103/api/Students', { name, age }, {headers: {"content-type": "application/json"}});
+//         clearForm();
+//         getAllStudents();
+//     }
+//     catch(error) {
+//         console.error("Error creating student: ", error);
+//     }
+// }
+// async function updateStudent(id: number, name: string, age: number) {
+//     try {
+//         await axios.put<StudentDTO>(`https://localhost:7103/api/Students/${id}`, { name, age }, { headers: {"content-type": "application/json"} });
+//         clearForm();
+//         getAllStudents();
+//     }
+//     catch(error) {
+//         console.error("Error updating student: ", error);
+//     }
+// }
+// saveButton.addEventListener('click', function() {
+//     const id = saveButton.dataset.id;
+//     if(id) {
+//         updateStudent(Number(id),studentName!.value, Number(studentAge!.value));
+//     }
+//     else {
+//         createStudent(studentName!.value, Number(studentAge!.value));
+//     }
+// });
+// async function deleteStudent(id: number) {
+//     try {
+//         await axios.delete(`https://localhost:7103/api/Students/${id}`);
+//         clearForm();
+//         getAllStudents();
+//     }
+//     catch(error) {
+//         console.error("Error deleting student: ", error);
+//     }
+// }
+// deleteButton.addEventListener('click', function() {
+//     const id = deleteButton.dataset.id;
+//     deleteStudent(Number(id));
+// });
+// getAllStudents();
--- Learn.Typescript/src/script.ts
+++ Learn.Typescript/src/script.ts
@@ -1,10 +1,20 @@
-// import axios from 'axios';
+// import { GridApi, GridOptions, createGrid, AllCommunityModule, ModuleRegistry } from '../node_modules/ag-grid-community';
+// import '../node_modules/ag-grid-community/styles/ag-grid.css'
+// import '../node_modules/ag-grid-community/styles/ag-theme-balham.css'
 
-const saveButton = document.querySelector<HTMLButtonElement>('#btnSave');
-const deleteButton = document.querySelector<HTMLButtonElement>('#btnDelete');
-const studentName = document.querySelector<HTMLInputElement>('#name');
-const studentAge = document.querySelector<HTMLInputElement>('#age');
-const studentsContainer = document.querySelector<HTMLDivElement>('#students_container')
+// Register all Community features
+// ModuleRegistry.registerModules([AllCommunityModule]);
+
+
+// const saveButton = document.querySelector<HTMLButtonElement>('#btnSave');
+// const deleteButton = document.querySelector<HTMLButtonElement>('#btnDelete');
+// const studentName = document.querySelector<HTMLInputElement>('#name');
+// const studentAge = document.querySelector<HTMLInputElement>('#age');
+// const studentsContainer = document.querySelector<HTMLDivElement>('#students_container')
+
+// if (!saveButton || !deleteButton || !studentName || !studentAge || !studentsContainer || !en {
+//     throw new Error("One or more required elements are missing in the DOM.");
+// }
 
 interface Student {
     id: number;
@@ -17,124 +27,148 @@ interface StudentDTO {
     age: number;
 }
 
-if (!saveButton || !deleteButton || !studentName || !studentAge || !studentsContainer) {
-    throw new Error("One or more required elements are missing in the DOM.");
-}
+const gridOptions = {
+    // Grid Options: Contains all of the Data Grid configurations
+    columnDefs: [
+        { headerName: 'Id', field: 'id', sortable: true, filter: true},
+        { headerName: 'Name', field: 'name', sortable: true, filter: true},
+        { headerName: 'Age', field: 'age', sortable: true, filter: true}
+    ],
+    rowData: [
+        { id: "1", name: "Kanellos", age: "45"},
+        { id: "2", name: "Kanell ts", age: "45"},
+        { id: "3", name: "Kanellos safas", age: "45"}
+    ]
+};
 
-async function clearForm() {
-    studentName!.value = '';
-    studentAge!.value = '';
-    deleteButton!.classList.add('hidden');
-}
+declare const agGrid : any;
+const studentsGrid = document.querySelector<HTMLDivElement>("#studentsGrid");
+const gridApi = agGrid.createGrid(studentsGrid, gridOptions);
 
-async function displayStudentInForm(student: { name: string; age: number; id: number; }) {
-    studentName!.value = student.name;
-    studentAge!.value = student.age.toString();
-    deleteButton!.classList.remove('hidden');
-    saveButton!.setAttribute('data-id',student.id.toString());
-    deleteButton!.setAttribute('data-id',student.id.toString());
-}
+// document.addEventListener('DOMContentLoaded', async function() {
+//     if(en {
+//         new (window as any).agGrid.Grid(en gridOptions);
+//         getAllStudents();
+//     }
+//     else {
+//         console.error("AG Grid container not found");
+//     }
+// });
 
-async function getStudentById(id: number) {
-    try {
-        const response = (await axios.get<Student>(`https://localhost:7103/api/Students/${id}`));
-        displayStudentInForm(response.data);
-    }
-    catch(error) {
-        console.error("Error fetching students: ", error);
-    }
-}
+// async function clearForm() {
+//     studentName!.value = '';
+//     studentAge!.value = '';
+//     deleteButton!.classList.add('hidden');
+// }
 
-function populateForm(id: number) {
-    getStudentById(id);
-}
+// async function displayStudentInForm(student: { name: string; age: number; id: number; }) {
+//     studentName!.value = student.name;
+//     studentAge!.value = student.age.toString();
+//     deleteButton!.classList.remove('hidden');
+//     saveButton!.setAttribute('data-id',student.id.toString());
+//     deleteButton!.setAttribute('data-id',student.id.toString());
+// }
 
-function displayStudents(students: Student[]) {
-    let allStudents = '';
+// async function getStudentById(id: number) {
+//     try {
+//         const response = (await axios.get<Student>(`https://localhost:7103/api/Students/${id}`));
+//         displayStudentInForm(response.data);
+//     }
+//     catch(error) {
+//         console.error("Error fetching students: ", error);
+//     }
+// }
 
-    students.forEach((student) => {
-        const studentElement = `
-                                    <div class="student" data-id="${student.id}">
-                                        <h3>${student.name}</h3>
-                                        <p>${student.age}</p>
-                                    </div>
-                                `;
+// function populateForm(id: number) {
+//     getStudentById(id);
+// }
+
+// function displayStudents(students: Student[]) {
+//     let allStudents = '';
+
+//     students.forEach((student) => {
+//         const studentElement = `
+//                                     <div class="student" data-id="${student.id}">
+//                                         <h3>${student.name}</h3>
+//                                         <p>${student.age}</p>
+//                                     </div>
+//                                 `;
         
-        allStudents += studentElement        
-    });
-    studentsContainer!.innerHTML = allStudents;
+//         allStudents += studentElement        
+//     });
+//     studentsContainer!.innerHTML = allStudents;
 
 
-    //add event listener
-    document.querySelectorAll('.student').forEach(student => {
-        student.addEventListener('click', async function() {
-            populateForm(Number(student!.getAttribute("data-id")));
-        });
-    });
-}
+//     //add event listener
+//     document.querySelectorAll('.student').forEach(student => {
+//         student.addEventListener('click', async function() {
+//             populateForm(Number(student!.getAttribute("data-id")));
+//         });
+//     });
+// }
 
-async function getAllStudents() {
-    try {
-        const response = await axios.get<Student[]>('https://localhost:7103/api/Students');
-        displayStudents(response.data);
-    } 
-    catch(error) {
-        console.error("Error fetching students: ", error);
-    }
-}
-
-
-async function createStudent(name: string, age: number) {
-    try {
-        await axios.post<StudentDTO>('https://localhost:7103/api/Students', { name, age }, {headers: {"content-type": "application/json"}});
-        clearForm();
-        getAllStudents();
-    }
-    catch(error) {
-        console.error("Error creating student: ", error);
-    }
-}
+// async function getAllStudents() {
+//     try {
+//         const response = await axios.get<Student[]>('https://localhost:7103/api/Students');
+//         displayStudents(response.data);
+//     } 
+//     catch(error) {
+//         console.error("Error fetching students: ", error);
+//     }
+// }
 
 
-async function updateStudent(id: number, name: string, age: number) {
-    try {
-        await axios.put<StudentDTO>(`https://localhost:7103/api/Students/${id}`, { name, age }, { headers: {"content-type": "application/json"} });
-        clearForm();
-        getAllStudents();
-    }
-    catch(error) {
-        console.error("Error updating student: ", error);
-    }
-}
+// async function createStudent(name: string, age: number) {
+//     try {
+//         await axios.post<StudentDTO>('https://localhost:7103/api/Students', { name, age }, {headers: {"content-type": "application/json"}});
+//         clearForm();
+//         getAllStudents();
+//     }
+//     catch(error) {
+//         console.error("Error creating student: ", error);
+//     }
+// }
 
 
-saveButton.addEventListener('click', function() {
-    const id = saveButton.dataset.id;
-
-    if(id) {
-        updateStudent(Number(id),studentName!.value, Number(studentAge!.value));
-    }
-    else {
-        createStudent(studentName!.value, Number(studentAge!.value));
-    }
-});
-
-
-async function deleteStudent(id: number) {
-    try {
-        await axios.delete(`https://localhost:7103/api/Students/${id}`);
-        clearForm();
-        getAllStudents();
-    }
-    catch(error) {
-        console.error("Error deleting student: ", error);
-    }
-}
-
-deleteButton.addEventListener('click', function() {
-    const id = deleteButton.dataset.id;
-    deleteStudent(Number(id));
-});
+// async function updateStudent(id: number, name: string, age: number) {
+//     try {
+//         await axios.put<StudentDTO>(`https://localhost:7103/api/Students/${id}`, { name, age }, { headers: {"content-type": "application/json"} });
+//         clearForm();
+//         getAllStudents();
+//     }
+//     catch(error) {
+//         console.error("Error updating student: ", error);
+//     }
+// }
 
 
-getAllStudents();
+// saveButton.addEventListener('click', function() {
+//     const id = saveButton.dataset.id;
+
+//     if(id) {
+//         updateStudent(Number(id),studentName!.value, Number(studentAge!.value));
+//     }
+//     else {
+//         createStudent(studentName!.value, Number(studentAge!.value));
+//     }
+// });
+
+
+// async function deleteStudent(id: number) {
+//     try {
+//         await axios.delete(`https://localhost:7103/api/Students/${id}`);
+//         clearForm();
+//         getAllStudents();
+//     }
+//     catch(error) {
+//         console.error("Error deleting student: ", error);
+//     }
+// }
+
+// deleteButton.addEventListener('click', function() {
+//     const id = deleteButton.dataset.id;
+//     deleteStudent(Number(id));
+// });
+
+
+// getAllStudents();
